e2aa2291844b7f52f9df6c35b1608ce404271ab6
Zoltus <zoltus@outlook.com>
2024-08-19 19:37:27 +0300

    Change theming to match github colors

--- my-theme-plugin/build.gradle.kts
+++ my-theme-plugin/build.gradle.kts
@@ -6,16 +6,14 @@ plugins {
 version = "0.1.0"
 
 fleetPlugin {
+    id = "fi.sulku.fleet.githubinspired"
 
-    // STEP 1: chose an ID for your plugin, it must be unique
-    //
-    // id = "your.plugin.id.here"
-
-    // STEP 2: set up the sensible metadata for your plugin
-    // metadata {
-    //     readableName = "Your plugin name here"
-    //     description = "Your plugin description here"
-    // }
+     metadata {
+         readableName = "Github Inspired - Dark"
+         description = "Github color theme inspired. Feel free to contribute!\n" +
+                       "If \"Add\" plugin doesn't work you can copy the theme to <home>/.fleet/themes/ " +
+                       "You can Also find the project on github \"Github-Inspired-Fleet-Theme\" by Zoltus"
+     }
 
     fleetRuntime {
         version = libs.versions.fleet.runtime
